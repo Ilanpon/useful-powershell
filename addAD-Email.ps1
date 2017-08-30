59afd537ce8a331e4a99ff3007784c53e7e381d4
ilanponimansky <31193909+ilanponimansky@users.noreply.github.com>
2017-08-30 13:08:13 -0400

    Update addAD-Email.ps1

--- addAD-Email.ps1
+++ addAD-Email.ps1
@@ -1,6 +1,8 @@
+Param(
+    [string]$emailDomain
+
 $uid = get-aduser  -filter * | select -expand samaccountname
-$emaildomain = "test.com"
 foreach ($name in $uid) {
-    write-host "$name@$emaildomain"
-    Set-ADUser -Identity $name -EmailAddress "$name@$emaildomain"
+    write-host "$name@$emailDomain"
+    Set-ADUser -Identity $name -EmailAddress "$name@$emailDomain"
 }
